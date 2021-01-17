a46f75ff69334d3eaea155ba6d81d1223605cfb5
Marcio Woitek <woitek@usp.br>
2021-01-17 01:57:20 -0300

    3rd script is complete.

--- project_1/plot3.R
+++ project_1/plot3.R
@@ -32,9 +32,50 @@ dat <-
     Date == as.Date("2007-02-01", format = "%Y-%m-%d") |
     Date == as.Date("2007-02-02", format = "%Y-%m-%d")
   ) %>%
+  select(Date, Time, Sub_metering_1, Sub_metering_2, Sub_metering_3) %>%
+  # Create a column by combining `Date` and `Time`:
+  mutate(date_time = as.POSIXct(paste(Date, Time))) %>%
+  select(!c(Date, Time))
 
-# Plot the x:
+# Translate the names of the weekdays into English:
+Sys.setlocale("LC_TIME", "en_US.utf8")
+
+# Plot the three time series:
 par(bg = NA)
-# plot
+# 1st time series:
+with(
+  dat,
+  plot(
+    x = date_time,
+    y = Sub_metering_1,
+    type = "l",
+    xlab = "",
+    ylab = "Energy sub metering"
+  )
+)
+# 2nd time series:
+with(
+  dat,
+  lines(
+    x = date_time,
+    y = Sub_metering_2,
+    col = "red"
+  )
+)
+# 3rd time series:
+with(
+  dat,
+  lines(
+    x = date_time,
+    y = Sub_metering_3,
+    col = "blue"
+  )
+)
+legend(
+  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+  lty = c(1, 1, 1),
+  col = c("black", "red", "blue"),
+  x = "topright"
+)
 dev.copy(png, file.path("my_figures", "plot3.png"))
 dev.off()
